c38d78428c331c31b05e89f3b506e47e1f111be1
nayemshaon34 <nayemshaon34@gmail.com>
2023-04-11 14:10:43 +0600

    github login updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from "firebase/auth"
+import {GithubAuthProvider, GoogleAuthProvider, getAuth, getRedirectResult, signInWithCredential, signInWithPopup, signInWithRedirect, signOut} from "firebase/auth"
 import app from "./firebase/firebase.init";
 import { useState } from "react";
 const auth = getAuth(app);
 
+
 function App() {
   const googleProvider = new GoogleAuthProvider();
+  const gitProvider = new GithubAuthProvider();
   const [user,newUser] = useState({});
   const handleSignInGoogle = () =>{
     signInWithPopup (auth,googleProvider)
@@ -30,6 +32,18 @@ function App() {
     })
   }
 
+  const hadnleSignInGithub = () =>{
+    signInWithPopup(auth,gitProvider)
+    .then(result =>{
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      newUser(user);
+      console.log(user);
+    })
+    }
+  
+
   return (
     <div className="App">
       {
@@ -37,13 +51,16 @@ function App() {
         <>
             <h2>Name : {user.displayName}</h2>
             <h3>Email: {user.email}</h3>
-            <img src={user.photoUrl} alt="" />
+            <img src={user.photoURL} alt="" />
             <h3>Email Verified: {user.emailVerified}</h3>
             <h3>Last SignInTime: {user.lastSignInTime}</h3>
             <button onClick={handleSignOut}>Sign Out</button>
         </>
         :
+        <>
         <button onClick={handleSignInGoogle} >Google Sign In</button>
+        <button onClick={hadnleSignInGithub} className="margin-between">Github Sign In</button>
+        </>
        }
     </div>
   );
